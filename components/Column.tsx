84972ceb4fff77d67d6da9b6aefd46153726942e
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 13:15:28 +0300

    column component

--- components/Column.tsx
+++ components/Column.tsx
@@ -7,6 +7,14 @@ type Props = {
   index: number;
 };
 
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  todo: "To Do",
+  inProgress: "In Progress",
+  done: "Done",
+};
+
 function Column({ id, todos, index }: Props) {
   return (
     <Draggable draggableId={id} index={index}>
@@ -25,7 +33,33 @@ function Column({ id, todos, index }: Props) {
                   snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
                 }`}
               >
-                <h2>{id}</h2>
+                <h2 className="flex justify-between font-bold text-xl">
+                  {idToColumnText[id]}
+                  <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-2 text-small font-normal">
+                    {todos.length}
+                  </span>
+                </h2>
+                <div className="space-y-2">
+                  {todos.map((todo, index) => (
+                    // <TodoCard />
+                    <Draggable
+                      key={todo.id}
+                      draggableId={todo.$id}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <TodoCard
+                          todo={todo}
+                          index={index}
+                          id={id}
+                          innerRef={provided.innerRef}
+                          draggableProps={provided.dragHandleProps}
+                          dragHandleProps={provided.dragHandleProps}
+                        />
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
               </div>
             )}
           </Droppable>
