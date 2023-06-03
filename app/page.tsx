0c8ab8f907be5b68c72c38ce67d3d9eb16f2a74a
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 09:53:33 +0300

    adding board component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      {/* <h1>Hello Boarder</h1> */}
-      {/* Header */}
       <Header />
 
-      {/* Board */}
+      <Board />
     </main>
   );
 }
