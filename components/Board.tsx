1392f40e316ccf815674562e72ae29e9c3b058d5
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 09:53:45 +0300

    adding dnd library

---
+++ components/Board.tsx
@@ -0,0 +1,21 @@
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+import React from "react";
+
+function Board() {
+  return (
+    <DragDropContext>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+            <div>
+                {/* render all the columns */}
+            </div>
+        )}
+
+
+      </Droppable>
+    </DragDropContext>
+  );
+}
+
+export default Board;
