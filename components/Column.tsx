bb589e3f15b7f393fad1b739c040b360a0a5fca1
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 13:39:50 +0300

    todo card component adding

--- components/Column.tsx
+++ components/Column.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
+import TodoCard from "./TodoCard";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   id: TypedColumn;
@@ -43,7 +45,7 @@ function Column({ id, todos, index }: Props) {
                   {todos.map((todo, index) => (
                     // <TodoCard />
                     <Draggable
-                      key={todo.id}
+                      key={todo.$id}
                       draggableId={todo.$id}
                       index={index}
                     >
@@ -53,12 +55,20 @@ function Column({ id, todos, index }: Props) {
                           index={index}
                           id={id}
                           innerRef={provided.innerRef}
-                          draggableProps={provided.dragHandleProps}
+                          draggableProps={provided.draggableProps}
                           dragHandleProps={provided.dragHandleProps}
                         />
                       )}
                     </Draggable>
                   ))}
+
+                  {provided.placeholder}
+
+                  <div className="flex items-end justify-end p-2">
+                    <button className="text-green-500 hover:text-green-600">
+                      <PlusCircleIcon className="h-10 w-10" />
+                    </button>
+                  </div>
                 </div>
               </div>
             )}
