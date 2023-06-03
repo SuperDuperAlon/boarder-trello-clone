6a447e2fd5b9279a93bf99c6bf0b7b7e9eb50761
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 12:35:22 +0300

    board updating

---
+++ typings.d.ts
@@ -0,0 +1,23 @@
+interface Board {
+  columns: Map<TypedColumn, Column>;
+}
+
+type TypedColumn = "todo" | "inProgress" | "done";
+
+interface Column {
+  id: TypedColumn;
+  todos: todo[];
+}
+
+interface Todo extends Models.Document {
+  $id: string;
+  $createdAt: string;
+  title: string;
+  status: TypedColumn;
+  image?: string;
+}
+
+interface Image {
+  bucketId: string;
+  fileId: string;
+}
