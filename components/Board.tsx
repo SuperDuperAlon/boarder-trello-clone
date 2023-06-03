7d0d4c642bba45f17809b0cf619e79c6ed27b1f5
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 14:06:47 +0300

    column drag and drop functionality

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,18 +6,32 @@ import { useBoardStore } from "@/store/BoardStore";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
 
-  console.log(board);
+    if (!destination) return;
+
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+  };
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
