01b6936f9696eef977b46629b85566659fc4d4f9
Alon Mlievski <alonmlievski@gmail.com>
2023-06-03 09:28:51 +0300

    adding gpt output design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import Avatar from "react-avatar";
 
@@ -32,8 +32,14 @@ function Header() {
           </form>
 
           {/* avatar */}
-          <Avatar name="Alon Mlievski" round size="50" color="#0055d1"/>
+          <Avatar name="Alon Mlievski" round size="50" color="#0055d1" />
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055d1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055d1] mr-1" />
+          GPT is summarizing your tasks for the day...
+        </p>
       </div>
     </header>
   );
